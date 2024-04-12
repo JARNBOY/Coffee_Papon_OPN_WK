e5d40394edd89d9b819f81a55cd1625226efa6ff
Papon <papon.s@lawyerplusgroup.com>
2024-04-12 15:31:42 +0700

    Initial Commit

---
+++ OPN _Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OPN _Challenge
+//
+//  Created by Papon Supamongkonchai on 12/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
