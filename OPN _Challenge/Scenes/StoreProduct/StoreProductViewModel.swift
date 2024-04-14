928278a137aa4401c899a3678f48b35ac6f3cd1c
Papon <papon.s@lawyerplusgroup.com>
2024-04-14 10:54:14 +0700

    start viewModel coordinator

---
+++ OPN _Challenge/Scenes/StoreProduct/StoreProductViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  StoreProductViewModel.swift
+//  OPN _Challenge
+//
+//  Created by Papon Supamongkonchai on 13/4/2567 BE.
+//
+
+import Foundation
+import SwiftUI
+
+final class StoreProductViewModel: ObservableObject {
+    
+}
