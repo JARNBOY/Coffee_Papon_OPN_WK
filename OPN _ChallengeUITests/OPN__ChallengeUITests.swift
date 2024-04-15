ccf5472ac3bb84c89dc11d75ef5e68677a30ea0e
Papon <papon.s@lawyerplusgroup.com>
2024-04-15 13:29:19 +0700

    Add UITest

--- OPN _ChallengeUITests/OPN__ChallengeUITests.swift
+++ OPN _ChallengeUITests/OPN__ChallengeUITests.swift
@@ -8,34 +8,35 @@
 import XCTest
 
 final class OPN__ChallengeUITests: XCTestCase {
+    
+    private var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
+        app.launchArguments = ["-UITests"]
         app.launch()
+    }
+    
+    func testLaunchScreen() {
+        // Wait for the splash screen to appear
+         let imageLogoSplashScreen = app.images[AccessibilityIdentifier.splashScreenImage.name]
+         XCTAssert(imageLogoSplashScreen.waitForExistence(timeout: 0.5))
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+         // Assert that the image is displayed correctly
+         XCTAssertTrue(imageLogoSplashScreen.exists)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testFullRegressionAppOPNChallenge() {
+        let elementsQuery = app.scrollViews.otherElements
+        elementsQuery.switches["Dark Tiramisu Mocha, Price: 75 Bath, 0"].buttons[AccessibilityIdentifier.plusProductButton.name].tap()
+        elementsQuery.switches["Dark Tiramisu Mocha, Price: 75 Bath, 1"].buttons[AccessibilityIdentifier.plusProductButton.name].tap()
+        elementsQuery.switches["Dark Tiramisu Mocha, Price: 75 Bath, 2"].buttons[AccessibilityIdentifier.plusProductButton.name].tap()
+        elementsQuery.switches["Latte, Price: 50 Bath, 0"].buttons[AccessibilityIdentifier.plusProductButton.name].tap()
+        elementsQuery.switches["Latte, Price: 50 Bath, 1"].buttons[AccessibilityIdentifier.plusProductButton.name].tap()
+        app.buttons[AccessibilityIdentifier.orderButton.name].tap()
+        app.buttons[AccessibilityIdentifier.confirmButton.name].tap()
+        app.buttons["Back to Order"].tap()
+        
     }
 }
